019a5bff3d45e02f9d0bcef93a777286951ee5fc
dmnyu <don.mennerich@nyu.edu>
2022-04-11 17:08:06 -0400

    Initial commit

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+)
+
+type Document struct {
+	XMLName  xml.Name `xml:"doc"`
+	Contents []Mixed  `xml:",any"`
+}
+type Mixed struct {
+	Type  string
+	Value interface{}
+}
+
+func main() {
+	bytes, err := ioutil.ReadFile("example.xml")
+	if err != nil {
+		panic(err)
+	}
+
+	var doc Document
+	if err := xml.Unmarshal([]byte(bytes), &doc); err != nil {
+		panic(err)
+	}
+
+	fmt.Println(doc)
+}
+
+func (m *Mixed) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	switch start.Name.Local {
+	case "head", "p", "list":
+		var e string
+		if err := d.DecodeElement(&e, &start); err != nil {
+			return err
+		}
+		m.Value = e
+		m.Type = start.Name.Local
+	default:
+		return fmt.Errorf("unknown element: %s", start)
+	}
+	return nil
+}
